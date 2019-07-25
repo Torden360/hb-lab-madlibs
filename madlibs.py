2e20497ebcbb401645edcc905f4f1d2929e33eff
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-24 17:23:44 -0700

    Created goodbye, game, madlib, and base.html templates. Passed the person variable through several templates, use the base template to auto-structure madlib.html

--- madlibs.py
+++ madlibs.py
@@ -41,6 +41,28 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+
+    playgame_input = request.args.get("playgame")
+    person = request.args.get("person")
+
+    if playgame_input == "yes":
+        return render_template("game.html", person=person)
+
+    else:
+        return render_template("goodbye.html")
+
+
+@app.route('/madlib')
+def show_madlib():
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adj = request.args.get("adj")
+    person = request.args.get("person")
+
+    return render_template("madlib.html",color=color, noun=noun, adjective=adj, person=person)
+
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
